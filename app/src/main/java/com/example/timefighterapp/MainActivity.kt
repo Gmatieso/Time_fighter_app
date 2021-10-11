0874aac5f363fcc5826a9f0504f0dcd49c116d47
Gmatieso <matiesogeoffrey@gmail.com>
2021-10-11 04:16:18 +0300

    added some reference button  line of code

--- app/src/main/java/com/example/timefighterapp/MainActivity.kt
+++ app/src/main/java/com/example/timefighterapp/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.timefighterapp
-
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    internal lateinit var myButton: Button
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
