e609c519da629ed9418ccf0f813a3143fa489f00
Gmatieso <matiesogeoffrey@gmail.com>
2021-10-11 05:24:22 +0300

    Commented the view properties

--- app/src/main/java/com/example/timefighterapp/MainActivity.kt
+++ app/src/main/java/com/example/timefighterapp/MainActivity.kt
@@ -6,12 +6,15 @@ import android.widget.TextView
 
 
 class MainActivity : AppCompatActivity() {
+    // reference to our button view properties
     internal lateinit var myButton: Button
+    //reference to our TextView properties
     internal lateinit var gameScoreTextView: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // populating our properties 
         myButton = findViewById(R.id.tap_me)
         gameScoreTextView = findViewById(R.id.gameScoreTextView)
     }
