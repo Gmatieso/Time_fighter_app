11a60a4592c43f6c933bf837b9aab7ba4ce6356a
Gmatieso <matiesogeoffrey@gmail.com>
2021-10-11 04:21:34 +0300

    made some changes to the internal reference textView

--- app/src/main/java/com/example/timefighterapp/MainActivity.kt
+++ app/src/main/java/com/example/timefighterapp/MainActivity.kt
@@ -2,11 +2,13 @@ package com.example.timefighterapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
+
 
 class MainActivity : AppCompatActivity() {
     internal lateinit var myButton: Button
-    internal lateinit var gameScoreTextView: Textview
-    
+    internal lateinit var gameScoreTextView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
